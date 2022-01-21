31e8787b9a3cb8d17ffbb7d1ea318e8c98676e5a
xwz723nn <xiwenzhang111@gmail.com>
2022-01-21 00:42:28 -0500

    Initial Commit

---
+++ first-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first-ios-app
+//
+//  Created by 张曦文 on 1/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first-ios-app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first-ios-app
+//
+//  Created by 张曦文 on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
