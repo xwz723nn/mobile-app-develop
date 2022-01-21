6c3cf54b83f44324e17d228f49dc8fc28f393076
xwz723nn <xiwenzhang111@gmail.com>
2022-01-21 03:30:19 -0500

    Initialize app

--- first-ios-app/first_ios_appApp.swift
+++ first-ios-app/first_ios_appApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct first_ios_appApp: App {
+    init() {
+        FirebaseApp.configure()
+        let ref = Database.database().reference()
+        ref.child("Name/student").setValue("Mike")
+    }
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
     }
 }
+
